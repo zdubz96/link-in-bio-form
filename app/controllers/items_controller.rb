a50328043c041d95248ac28c7aa883fd0b58240f
zdubz96 <zchan@chicagobooth.edu>
2025-04-30 22:58:03 +0000

    V1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,19 @@
 class ItemsController < ApplicationController
   def index
     @list_of_items = Item.all
-
     render({ :template => "item_templates/list" })
+  end
+
+  def new
+    render({ :template => "item_templates/form" })
+  end
+
+  def add_item
+    new_item=Item.new
+    new_item.link_url=params[:url]
+    new_item.link_description=params[:description]
+    new_item.thumbnail_url=params[:image]
+    new_item.save
+    redirect_to("/")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "new" })
+  post("/process_inputs",{:controller=>"items",:action=> "add_item"})
 end
